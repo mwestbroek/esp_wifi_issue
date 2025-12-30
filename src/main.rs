b211519d5c29d3dd4c177768bc5cd4b84f522ad0
Maurits Westbroek <maurits@hoxtonfarms.com>
2025-12-30 16:54:09 +0000

    Add an interrupt task

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,22 @@ use esp_hal::{
     interrupt::{software::SoftwareInterruptControl, Priority},
 };
 use esp_hal::timer::timg::TimerGroup;
+use static_cell::StaticCell;
+use esp_rtos::embassy::InterruptExecutor;
+use embassy_time::{Duration, Timer};
 
 
 mod wifi;
 
 esp_bootloader_esp_idf::esp_app_desc!();
+
+#[embassy_executor::task]
+async fn interrupt_task() {
+    loop {
+        println!("Interrupt task running");
+        Timer::after(Duration::from_millis(500)).await;
+    }
+}
 
 
 #[esp_rtos::main]
@@ -34,6 +45,14 @@ async fn main(low_priority_spawner: Spawner) {
     println!("Initializing WiFi...");
     let stack = wifi::get_wifi(low_priority_spawner, peripherals.WIFI).await;
     println!("WiFi initialized.");
+
+    static EXECUTOR: StaticCell<InterruptExecutor<2>> = StaticCell::new();
+    let executor = InterruptExecutor::new(sw_ints.software_interrupt2);
+    let executor = EXECUTOR.init(executor);
+
+    let spawner = executor.start(Priority::Priority3);
+    // The interrupt executor can interrupt uncooperative tasks
+    spawner.must_spawn(interrupt_task());
 
 }
 
